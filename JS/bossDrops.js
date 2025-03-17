5097255693b18aea5ce77602c9af463396219655
Sean Rexford <sprexford@gmail.com>
2025-03-17 13:34:11 -0700

    Create function to roll item quantities

--- JS/bossDrops.js
+++ JS/bossDrops.js
@@ -4,6 +4,10 @@ function rollRandomNumber(max) {
   return Math.floor(Math.random() * max) + 1;
 }
 
+function rollItemQuantity(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 //Now we gotta set up the bosses. To keep it in line with OSRS, unfortunatly every boss gets it's own individual drop tables.
 
 const bosses = {};
